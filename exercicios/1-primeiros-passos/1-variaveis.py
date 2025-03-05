0875e40184687564ab045d29318dfd8478c10d44
Pedro-Tux <pedroallexandrino@gmail.com>
2025-03-05 01:09:25 +0000

    Realizado exerciicio

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,16 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = 'Pedro'
+cargo = "Analista"
+objetivo = '''
+Aprender Python
+para mudar de profissão!
+'''
 # Declare variáveis de cada tipo numérico
-
+idade = 55
+altura = 1.79
 
 # Declare uma variável do tipo booleano
+casado = True
+filhos = False
